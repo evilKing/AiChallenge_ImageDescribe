b116182b0ed28c5b2aa799f3f2479e45735bcb82
yinhao <yinhao@YINHAO.pycredit.cn>
2017-09-07 18:03:20 +0800

    读写文件

--- src/com/evilking/pre/BuildTrainSet.java
+++ src/com/evilking/pre/BuildTrainSet.java
@@ -1,10 +1,40 @@
 package com.evilking.pre;
 
+import java.io.File;
+
 public class BuildTrainSet {
 
+	private static String TRAIN_PATH = "E:\\迅雷下载\\ai_challenger_caption_train_20170902";
+	
+	private static String ROOT_PATH = "data//";
+	
+	public static void readFiles(String filename){
+		File file = new File(TRAIN_PATH);
+		if(!file.exists()){
+			System.out.println(file.getPath() + " : 文件不存在!");
+		}
+		if(file.isDirectory()){
+			//图像目录
+			File[] images = file.listFiles();
+		
+		}else{
+			//json文件
+			
+			
+		}
+		
+		
+		
+	}
+	
+	public static void writeFiles(String filename){
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
